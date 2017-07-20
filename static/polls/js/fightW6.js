811b61744b4a15a3b4f63829ceb67c70e6b52de0
guo <artofree@me.com>
2017-07-20 13:07:16 +0800

    .

--- static/polls/js/fightW6.js
+++ static/polls/js/fightW6.js
@@ -11,9 +11,8 @@ var heartbeatTimer;
 
 function checkHeartBeat(){
     var myDate = new Date();
-    theTime = myDate.getTime();
     //如果每次计时的5秒间隔里没有来心跳，提示刷新
-    if (theTime -heartbeatTime >5000){
+    if (myDate.getTime() -heartbeatTime >5000){
         $("#title").html('连接已断开，请刷新!');
         clearInterval(heartbeatTimer);
     }
@@ -173,95 +172,3 @@ $(document).ready(function () {
         }
     });
 });
-
-
-//function getCodeImg() {
-//    $.get(parms.getCodeImg, function (ret) {
-//        theList = ret.split('-');
-//        ///暂无验证
-//        if (theList[0] == '0') {
-//            setTimeout("getCodeImg()", 3000);
-//        }
-//        else if (theList[0] == '1') {
-//            setTimeout("getCodeImg()", 200);
-//        }
-//        ///验证状态中
-//        else if (theList[0] == '1') {
-//            ///状态切换，开始倒计时
-//            if (!isActive) {
-//                isActive = 1;
-//                $("#coreContent").html('<h2 id="train-img" style="width: 100%;color: black">验证将在<mark id="countdown"></mark>秒后开始</h2>');
-//                $("#train-img").height(imgHeight);
-//                $("#countdown").text(theList[1]);
-//                setTimeout("getCodeImg()", 200);
-//            }
-//            else {
-//                ///未上传验证图，继续倒计时
-//                if (theList[2] == '') {
-//                    $("#countdown").text(theList[1]);
-//                    setTimeout("getCodeImg()", 200);
-//                }
-//                ///开始解码
-//                else {
-//                    var codeTimes = '';
-//                    if (codetimes == 0) {
-//                        codeTimes = '<h2 style="color: black">第一码：</h2>';
-//                    }
-//                    else {
-//                        codeTimes = '<h2 style="color: black">第二码：</h2>';
-//                    }
-//
-//                    $("#coreContent").html(codeTimes + '<img id="train-img" class="img-responsive" src="" style="width: 100%;margin-bottom: 10px">');
-//                    $("#train-img").attr("src", theList[2]);
-//                    $("#train-img").css({
-//                        "display": "inline-block",
-//                        "width": "400px",
-//                        "height": "240px",
-//                        "margin-bottom": "50px"
-//                    });
-//                    idt = theList[3];
-//                    isWork = 1;
-//                    codetimes += 1;
-//                    var myDate = new Date();
-//                    theTime = myDate.getTime();
-//                    theTimer = setInterval('changeButton()', 1000);
-//                }
-//            }
-//        }
-//    })
-//}
-
-
-//$(document).ready(function () {
-//    $("#train-input").keydown(function (event) {
-//        if (event.which == '13') {
-//            if (isWork) {
-//                inputString = $("#train-input").val();
-//                ///setCode
-//                $.post(parms.setCode,
-//                    {
-//                        idt: idt,
-//                        code: inputString
-//                    },
-//                    function (data, status) {
-//                    });
-//                var myDate = new Date();
-//                if (codetimes == 1) {
-//                    firstTime = (myDate.getTime() - theTime).toString();
-//                    firstCode = inputString;
-//                    train_timer = 0;
-//                    clearInterval(theTimer);
-//                    setTimeout("getCodeImg()", 200);
-//                }
-//                else {
-//                    var theContent = (myDate.getTime() - theTime).toString();
-//                    theContent = "<h3>你的第二码答案为：" + inputString + "</h3><br><strong>用时： " + theContent + " 毫秒</strong>";
-//                    theContent = "<br><br><h3>你的第一码答案为：" + firstCode + "</h3><br><strong>用时： " + firstTime + " 毫秒</strong>" + theContent;
-//                    $("#modalContent").html(theContent);
-//                    $('#myModal').modal();
-//                    $("#close_button").focus();
-//                }
-//            }
-//        }
-//    });
-//});
